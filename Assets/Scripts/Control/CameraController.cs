d0bf5143d88c047a6394865ef2ce8d7bfef019ff
GMihalkow <georgimihalkov@abv.bg>
2022-11-06 20:18:21 +0200

    Implemented base camera controller logic

---
+++ Assets/Scripts/Control/CameraController.cs
@@ -0,0 +1,31 @@
+﻿namespace ParkingSimultaor.Control
+{
+    using UnityEngine;
+
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed = 10f;
+        [SerializeField] private Transform _target;
+        [SerializeField] Vector3 _offset = Vector3.back * 5f + Vector3.up * 3f;
+
+        private float _xAngle;
+        private Transform _cachedTransform;
+
+        private void Awake() => _cachedTransform = transform;
+
+        private void Start() => Cursor.lockState = CursorLockMode.Locked;
+
+        private void Update()
+        {
+            var xInput = Input.GetAxis("Mouse X");
+
+            _xAngle += xInput * _rotationSpeed * Time.deltaTime;
+
+            var yRot = Quaternion.AngleAxis(_xAngle, Vector3.up);
+            var direction = _target.position - _cachedTransform.position;
+
+            _cachedTransform.position = _target.position + yRot * _offset;
+            _cachedTransform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+    }
+}
