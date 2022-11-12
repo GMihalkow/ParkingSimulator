acd66c18f814a33d328c4daeb275a06f0b40bb96
GMihalkow <georgimihalkov@abv.bg>
2022-11-12 15:22:16 +0200

    Implemented car rotation validation

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -16,28 +16,18 @@ namespace ParkingSimultaor.Control
         [SerializeField] private Transform _frontRightTransform;
         [SerializeField] private float _brakeDuration;
         [SerializeField] private float _brakeMaxForce = 50f;
+        [SerializeField] private float _fallOverResetTimeout = 2f;
 
         private float _brakeStartTime;
         private Vector2 _input;
+        private float _fellOverAt = -1;
 
         private void Update()
         {
             _input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _brakeStartTime = Time.time;
-            }
-            else if (Input.GetKey(KeyCode.Space))
-            {
-                _frontLeftWheel.brakeTorque = Mathf.Lerp(0f, _brakeMaxForce, (Time.time - _brakeStartTime) / _brakeDuration);
-                _frontRightWheel.brakeTorque = Mathf.Lerp(0f, _brakeMaxForce, (Time.time - _brakeStartTime) / _brakeDuration);
-            }
-            else if (Input.GetKeyUp(KeyCode.Space))
-            {
-                _frontLeftWheel.brakeTorque = 0f;
-                _frontRightWheel.brakeTorque = 0f;
-            }
+            HandleBrakes();
+            ValidatePosition();
         }
 
         private void FixedUpdate()
@@ -62,6 +52,42 @@ namespace ParkingSimultaor.Control
 
             wheelTransform.position = pos;
             wheelTransform.rotation = rot;
+        }
+
+        private void HandleBrakes()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _brakeStartTime = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.Space))
+            {
+                _frontLeftWheel.brakeTorque = Mathf.Lerp(0f, _brakeMaxForce, (Time.time - _brakeStartTime) / _brakeDuration);
+                _frontRightWheel.brakeTorque = Mathf.Lerp(0f, _brakeMaxForce, (Time.time - _brakeStartTime) / _brakeDuration);
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                _frontLeftWheel.brakeTorque = 0f;
+                _frontRightWheel.brakeTorque = 0f;
+            }
+        }
+
+        private void ValidatePosition()
+        {
+            var upDot = Vector3.Dot(transform.up, Vector3.up);
+
+            if (upDot <= 0f && _fellOverAt < 0)
+            {
+                _fellOverAt = Time.time;
+            }
+            else if (_fellOverAt > 0 && Time.time -_fellOverAt > _fallOverResetTimeout)
+            {
+                _fellOverAt = -1;
+
+                var zDiff = Quaternion.FromToRotation(transform.up, Vector3.up);
+
+                transform.rotation *= zDiff;
+            }
         }
     }
 }
