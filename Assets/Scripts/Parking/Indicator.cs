413f81cb04bff6fcbf113a6262bd926805d9360d
GMihalkow <georgimihalkov@abv.bg>
2022-11-12 13:51:03 +0200

    Implemented parking slot prefab

---
+++ Assets/Scripts/Parking/Indicator.cs
@@ -0,0 +1,27 @@
+﻿namespace ParkingSimultaor.Parking
+{
+    using UnityEngine;
+
+    public class Indicator : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private float _travelDistance = 2f;
+
+        private Vector3 _initialPos;
+        private float _angle;
+
+        private void Awake() =>_initialPos = transform.position;
+
+        private void Update()
+        {
+            if (_angle > 360f)
+            {
+                _angle -= 360f;
+            }
+
+            transform.position = _initialPos + Vector3.up * _travelDistance * Mathf.Sin(_angle);
+
+            _angle += _speed * Time.deltaTime;
+        }
+    }
+}
