cd2df6da9745178cb371509b1902618b4c1fdcb9
GMihalkow <georgimihalkov@abv.bg>
2022-11-24 21:45:54 +0200

    Deleted unused scene and implemented base main menu logic

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿namespace ParkingSimultaor.UI
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private string _gameSceneName = "MainScene";
+
+        /// <summary>
+        /// Called from editor script
+        /// </summary>
+        public void OnPlayBtnClick() => SceneManager.LoadScene(_gameSceneName, LoadSceneMode.Single);
+
+        /// <summary>
+        /// Called from editor script
+        /// </summary>
+        public void OnExitBtnClick() => Application.Quit();
+    }
+}
