db77a6f47a9b433e83381d3cd339016c3a059224
GMihalkow <georgimihalkov@abv.bg>
2022-11-27 16:36:01 +0200

    Implemented helper markers

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,19 +2,28 @@ namespace ParkingSimultaor.Control
 {
     using ParkingSimultaor.Car;
     using ParkingSimultaor.Scripts.UI;
+    using ParkingSimultaor.Utilities;
     using UnityEngine;
     using UnityEngine.SceneManagement;
+    using UnityEngine.UI;
 
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] private CarController _carController;
         [SerializeField] private PauseMenu _pauseMenu;
         [SerializeField] private string _mainMenuScene = "MenuScene";
+        [SerializeField] private ObjectScanner _parkingSlotsScanner;
+        [SerializeField] private Image _marker;
+        [SerializeField] private float _updateMarkerFrequency = 0.02f;
 
+        private float _lastMarkerUpdate = -1;
         private Vector2 _input;
+        private Camera _mainCamera;
+        private bool _showHelperMarkers;
 
         private void Awake()
         {
+            _mainCamera = Camera.main;
             _carController.Initialize(transform);
             _pauseMenu.onResumeBtnClick += OnPauseMenuResumeBtnClick;
             _pauseMenu.onExitBtnClick += OnPauseMenuExitBtnClick;
@@ -37,9 +46,70 @@ namespace ParkingSimultaor.Control
             {
                 OnPauseMenuResumeBtnClick();
             }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                _showHelperMarkers = !_showHelperMarkers;
+
+                if (!_showHelperMarkers)
+                {
+                    _marker.gameObject.SetActive(false);
+                }
+            }
+
+            if (_showHelperMarkers && (_updateMarkerFrequency < 0 || (Time.time - _lastMarkerUpdate > _updateMarkerFrequency)))
+            {
+                PlaceMarker();
+            }
         }
 
         private void FixedUpdate() => _carController.OnFixedUpdate(ref _input);
+
+        private void PlaceMarker()
+        {
+            if (_parkingSlotsScanner.results.Length > 0)
+            {
+                var flatCameraPos = _mainCamera.transform.position;
+                flatCameraPos.y = 0f;
+
+                var flatDirection = _mainCamera.transform.forward;
+                flatDirection.y = 0f;
+
+                var finalIndex = 0;
+                var closestDot = default(float?);
+
+                for (int index = 0; index < _parkingSlotsScanner.results.Length; index++)
+                {
+                    if (_parkingSlotsScanner.results[index] == null) continue;
+
+                    var colliderFlatPos = _parkingSlotsScanner.results[index].transform.position;
+                    colliderFlatPos.y = 0f;
+
+                    var flatDirTowardsTarget = (colliderFlatPos - flatCameraPos).normalized;
+                    var dot = Vector3.Dot(flatDirection, flatDirTowardsTarget);
+
+                    if (!closestDot.HasValue)
+                    {
+                        closestDot = dot;
+                        finalIndex = index;
+                    }
+                    else if (closestDot < dot)
+                    {
+                        closestDot = dot;
+                        finalIndex = index;
+                    }
+                }
+
+                _marker.gameObject.SetActive(true);
+                _marker.transform.position = _mainCamera.WorldToScreenPoint(_parkingSlotsScanner.results[finalIndex].transform.position);
+            }
+            else
+            {
+                _marker.gameObject.SetActive(false);
+            }
+
+            _lastMarkerUpdate = Time.time;
+        }
 
         private void OnPauseMenuResumeBtnClick()
         {
---
+++ Assets/Scripts/Utilities/ObjectScanner.cs
@@ -0,0 +1,42 @@
+﻿namespace ParkingSimultaor.Utilities
+{
+    using UnityEngine;
+
+    public class ObjectScanner : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _targetLayers;
+        [SerializeField, Min(0f)] private float _scanRadius = 5f;
+        [SerializeField] private bool _drawGizmos;
+        [SerializeField, Min(0f)] private float _scanFrequency = 0.02f;
+        [SerializeField, Min(0)] private int _maxResultsCount = 3;
+
+        public Collider[] results => _results;
+
+        private Collider[] _results;
+        private float _lastScan = -1;
+
+        private void Awake() => _results = new Collider[_maxResultsCount];
+
+        private void Update()
+        {
+            if (_lastScan < 0 || (_lastScan >= 0f && Time.time - _lastScan > _scanFrequency))
+            {
+                Scan();
+            }
+        }
+
+        private int Scan()
+        {
+            _lastScan = Time.time;
+
+            return Physics.OverlapSphereNonAlloc(transform.position, _scanRadius, _results, _targetLayers);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!_drawGizmos) return;
+
+            Gizmos.DrawWireSphere(transform.position, _scanRadius);
+        }
+    }
+}
