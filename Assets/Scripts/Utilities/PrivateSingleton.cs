ead5e5a554ed39783c44418440eb32d44d64d6aa
GMihalkow <georgimihalkov@abv.bg>
2023-01-10 23:05:43 +0200

    Implemented PrivateSingleton

---
+++ Assets/Scripts/Utilities/PrivateSingleton.cs
@@ -0,0 +1,22 @@
+﻿namespace ParkingSimulator.Utilities
+{
+    using UnityEngine;
+
+    public class PrivateSingleton<T> : MonoBehaviour
+    {
+        public static T instance => _instance;
+
+        protected static T _instance;
+
+        protected virtual void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            _instance = this.GetComponent<T>();
+        }
+    }
+}
